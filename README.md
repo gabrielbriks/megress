4678360ba4c9ea36b0032875a149fe6e311ae38f
Gabriel Morais <greis9010@gmail.com>
2024-09-14 17:09:17 -0300

    (feat): Implementa fluxo de cadastro de metas(goals)

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Techs And Main Dependencies
 
+- Node.js
+- Fastify
 - Typescript
 - Tsx
 - Zod
@@ -12,3 +14,11 @@
 - Postgres(dependence & DB)
 - @paralleldrive/cuid2
 - DayJS
+
+
+
+
+## Learnings
+
+#### Validações
+- Para validações das rotas/requets, foi utilizado o ["fastify-type-provider-zod"](https://github.com/turkerdev/fastify-type-provider-zod), que proporciona um fluxo um pouco mais automatizado.
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,41 @@
-import fastify from "fastify";
+import fastify from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import { createGoal } from '../services/create-goal';
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string().min(1).max(255),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body;
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    });
+  }
+);
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("HTTP server running");
+    console.log('HTTP server running');
   });
